5dc78f0c98a746c9c472ebb27bf2fd6eae2021f7
Sarvesh Mhadgut <sarveshmhadgut@icloud.com>
2026-07-09 12:56:40 +0530

    update 02_ltm_postgres.ipynb & add new utils to langgraph_utils

--- LanggraphSandbox/langgraph_utils/__init__.py
+++ LanggraphSandbox/langgraph_utils/__init__.py
@@ -1,5 +1,33 @@
-from rich.console import Console
+import os
+from typing import List
 from rich.theme import Theme
+from termcolor import colored
+from rich.console import Console
+from rich.markdown import Markdown
+from langchain_core.runnables import RunnableConfig
+from langchain_core.messages import BaseMessage, AIMessage, HumanMessage
+
+
+def display_conversation(messages: List[BaseMessage]):
+    for message in messages:
+        print(colored("LLM:", (193, 162, 255), ["bold"])) if isinstance(message, AIMessage) else print(colored("\nHuman:", (222, 207, 166), ["bold"]))
+
+        console.print(Markdown(f"{message.content}\n"))
+
+
+def get_config(user_id, thread_id):
+    return RunnableConfig(
+        {
+            "configurable": {"thread_id": thread_id, "user_id": user_id},
+            "metadata": {
+                "thread_id": thread_id,
+                "user_id": user_id,
+                "environment": os.getenv("APP_ENV", "default"),
+                "app": "ltm_postgres",
+            },
+        },
+        run_name="ltm_postgres_run",
+    )
 
 
 def get_console() -> Console:
